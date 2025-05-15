942c8d3fcd91bb2948e5fb739628b82d05cdd43c
Jared Updike <jupdike@thebrain.com>
2025-05-15 13:20:09 -0700

    BrayElem.renderToString: gathers up headings for Table of Contents

--- src/BrayElem.js
+++ src/BrayElem.js
@@ -70,6 +70,24 @@ export default class BrayElem {
   static create(type, props, ...children) {
     return new BrayElem(type, props, ...children);
   }
+  duplicateWithField(fieldKey, value) {
+    let nu = {};
+    nu[fieldKey] = value;
+    let props = BrayElem.mergeObjects(this.props, nu); // includes children
+    let ret = BrayElem.create(this.type, props, ...props.children);
+    // console.warn('-=-=-');
+    // console.warn(this);
+    // console.warn(ret);
+    return ret;
+  }
+  invokeSelf(moreProps) {
+    if (BrayElem.isString(this.type)) {
+      return this;
+    }
+    let props = BrayElem.mergeObjects(this.props, moreProps);
+    // invoke the component (assumes it is a function if it is not a string)
+    return this.type(props);
+  }
   // merge all k:v pairs in old and nu into brand new object (nu supersedes old)
   // either can be null or undefined
   static mergeObjects(old, nu) {
@@ -92,16 +110,6 @@ export default class BrayElem {
     }
     // console.warn('---');
     // console.warn(old);
-    // console.warn(ret);
-    return ret;
-  }
-  duplicateWithField(fieldKey, value) {
-    let nu = {};
-    nu[fieldKey] = value;
-    let props = BrayElem.mergeObjects(this.props, nu); // includes children
-    let ret = BrayElem.create(this.type, props, ...props.children);
-    // console.warn('-=-=-');
-    // console.warn(this);
     // console.warn(ret);
     return ret;
   }
@@ -130,68 +138,12 @@ export default class BrayElem {
   static get MAX_COMPONENT_DEPTH() {
     return 10000;
   }
-  // Components are computed lazily, so reduce them to get to a final "atomic" (string) type
-  _reduceHead(moreProps) {
-    let ret = this;
-    let count = 0;
-    while (!BrayElem.isString(ret.type)) {
-      ret = ret.type(BrayElem.mergeObjects(ret.props, moreProps));
-      // detect inadvertent infinite loops
-      count++;
-      if (count > BrayElem.MAX_COMPONENT_DEPTH) {
-        throw "BrayElem.render -- max component depth exceeded: " + BrayElem.MAX_COMPONENT_DEPTH;
-      }
-    }
-    //console.log('_reduceHead ret:', ret);
-    return ret;
-  }
   static pushFront(oldArray, item) {
     let arr = oldArray;
     if (!arr) {
       arr = [];
     }
     return [item].concat(arr);
-  }
-  invokeSelf(moreProps) {
-    if (BrayElem.isString(this.type)) {
-      return this;
-    }
-    let props = BrayElem.mergeObjects(this.props, moreProps);
-    // invoke the component (assumes it is a function if it is not a string)
-    return this.type(props);
-  }
-  renderToString() {
-    let builder = [];
-    let indentBox = { indent: 0 };
-    if (BrayElem.isString(this.type)) {
-      this._renderToStringInner(indentBox, builder);
-    }
-    else {
-      this._reduceHead()._renderToStringInner(indentBox, builder);
-    }
-    let ret = builder.join('');
-    return ret;
-  }
-  static _smartBuilderPush(builder, indentBox, item) {
-    //console.log('sBI:', indentBox.indent, '|||'+item+'|||');
-    if (item.startsWith('</')) {
-      indentBox.indent--;
-    }
-    //console.log('sBI:', indentBox.indent);
-    let lastClosed = builder.length > 0 && builder[builder.length - 1].endsWith(">");
-    if (lastClosed && item.startsWith('<')) {
-      //console.log('sBI -- newline and indent');
-      builder.push('\n');
-      for (let i = 0; i < indentBox.indent; i++) {
-        builder.push('  ');
-      }
-    }
-    //console.log('sBI:', indentBox.indent);
-    if (item.startsWith('<') && !item.startsWith('</') && !item.endsWith('/>')) {
-      indentBox.indent++;
-    }
-    //console.log('sBI:', indentBox.indent);
-    builder.push(item);
   }
   static escapedString(x) {
     // TODO escape quotes and backslashes
@@ -222,67 +174,23 @@ export default class BrayElem {
     });
     return builder;
   }
-  // Don't call this directly from outside renderToString, please
-  // cf. https://stackoverflow.com/questions/22156326/private-properties-in-javascript-es6-classes
-  _renderToStringInner(indentBox, builder) {
-    if (!BrayElem.isString(this.type)) {
-      throw 'BrayElem.renderInner -- expected string element type; this should not happen ...';
+  static _pairListToTreeList(pairs, cons) {
+    if (pairs.length === 0) {
+      return [];
     }
-    let attrBuilder = [];
-    for (let k in this.props) {
-      if (!this.props.hasOwnProperty(k) || k === 'children') {
-        continue;
-      }
-      let rhs = this.props[k] + ''; // make this a string
-      attrBuilder.push(` ${k}="${BrayElem.escapedString(rhs)}"`);
+    const [{ level, text }, ...rest] = pairs;
+    const kids = [];
+    let i = 0;
+    // Find all children (where level > current level)
+    while (i < rest.length && rest[i].level > level) {
+      kids.push(rest[i]);
+      i++;
     }
-    let attrString = attrBuilder.join('');
-    if (this.props.children.length < 1) {
-      BrayElem._smartBuilderPush(builder, indentBox, `<${this.type}${attrString}/>`);
-    }
-    else {
-      if (this.type === BrayElem.Fragment) {
-        this._renderChildrenToString(builder, indentBox, this.props.children);
-      }
-      else {
-        BrayElem._smartBuilderPush(builder, indentBox, `<${this.type}${attrString}>`);
-        this._renderChildrenToString(builder, indentBox, this.props.children);
-        BrayElem._smartBuilderPush(builder, indentBox, `</${this.type}>`);
-      }
-    }
-  }
-  _renderChildrenToString(builder, indentBox, children) {
-    children.forEach(kid => {
-      if (BrayElem.isString(kid)) {
-        BrayElem._smartBuilderPush(builder, indentBox, kid);
-      }
-      // an array is from fragments?
-      else if (BrayElem.isArray(kid)) {
-        this._renderChildrenToString(builder, indentBox, kid);
-      }
-      else {
-        kid._reduceHead()._renderToStringInner(indentBox, builder);
-      }
-    });
-  }
-  // 'sax' here is the sax-js namespace (sax.parser will be created for us by XmlHandler)
-  // retrieved 4 Feb 2022 -- last change 5aee216 on Jun 22, 2017
-  // https://github.com/isaacs/sax-js
-  // https://raw.githubusercontent.com/isaacs/sax-js/master/lib/sax.js
-  // https://github.com/isaacs/sax-js/blob/master/lib/sax.js
-  //
-  // 'xmlString' is the XML document as a single string (one XML element (with children), sans DOCTYPE, sans ?xml)
-  //
-  // 'tagMap' is an optional object with keys (strings) mapping tag names to either strings or
-  // function components. Tag name strings with no matching key in 'tagMap' will be left as is.
-  // Function components here mean functions that take a single argument, props (with props.children),
-  // and return a BrayElem. The result of these optional function components (BrayElem(s)) can then be
-  // rendered back to an XML string via BrayElem.renderToString()
-  static fromXmlString(sax, xmlString, tagMap) {
-    tagMap = tagMap || {};
-    const handler = new XmlHandler(sax, tagMap);
-    // parse xmlString and create a giant tree of BrayElems
-    return handler.parseXmlString(xmlString);
+    const sibs = rest.slice(i);
+    return [
+      cons(text, BrayElem._pairListToTreeList(kids, cons)),
+      ...BrayElem._pairListToTreeList(sibs, cons)
+    ];
   }
   // fetch a value:
   // 0. if props is actually an array, iterate through and try to find the first matching key (1, 2, 3)
@@ -332,5 +240,150 @@ export default class BrayElem {
       }
     }
     return defaultValue;
+  }
+  // Components are computed lazily, so reduce them to get to a final "atomic" (string) type
+  _reduceHead(moreProps) {
+    let ret = this;
+    let count = 0;
+    while (!BrayElem.isString(ret.type)) {
+      ret = ret.type(BrayElem.mergeObjects(ret.props, moreProps));
+      // detect inadvertent infinite loops
+      count++;
+      if (count > BrayElem.MAX_COMPONENT_DEPTH) {
+        throw "BrayElem.render -- max component depth exceeded: " + BrayElem.MAX_COMPONENT_DEPTH;
+      }
+    }
+    //console.log('_reduceHead ret:', ret);
+    return ret;
+  }
+  static _smartBuilderPush(renderState, item) {
+    let builder = renderState.builder;
+    //console.log('sBI:', indentBox.indent, '|||'+item+'|||');
+    if (item.startsWith('</')) {
+      renderState.indent--;
+    }
+    //console.log('sBI:', renderState.indent);
+    let lastClosed = builder.length > 0 && builder[builder.length - 1].endsWith(">");
+    if (lastClosed && item.startsWith('<')) {
+      //console.log('sBI -- newline and indent');
+      builder.push('\n');
+      for (let i = 0; i < renderState.indent; i++) {
+        builder.push('  ');
+      }
+    }
+    //console.log('sBI:', renderState.indent);
+    if (item.startsWith('<') && !item.startsWith('</') && !item.endsWith('/>')) {
+      renderState.indent++;
+    }
+    //console.log('sBI:', renderState.indent);
+    builder.push(item);
+  }
+  renderToString(outer = true) {
+    let renderState = {
+      indent: 0,
+      builder: [],
+      headings: [],
+    };
+    function processHeader(elem) {
+      let text = elem.props.children.map(
+        x => BrayElem.isString(x)
+        ? x
+        : (x.renderToString ? x.renderToString(false) : '')
+      ).join('');
+      if (text.match(/^\[@PRE-TOC@\]/g)) {
+        //console.warn('@TOC@ found, so headings are reset up to that point in the document');
+        renderState.headings = [];
+      }
+      if (elem.type === 'h1'
+        || elem.type === 'h2'
+        || elem.type === 'h3'
+        || elem.type === 'h4'
+        || elem.type === 'h5'
+        || elem.type === 'h6')
+      {
+        let level = parseInt(elem.type[1]);
+        renderState.headings.push({ level: level, text: text });
+      }
+    }
+    renderState.processHeader = processHeader;
+    if (BrayElem.isString(this.type)) {
+      this._renderToStringInner(renderState);
+    }
+    else {
+      this._reduceHead()._renderToStringInner(renderState);
+    }
+    // transform headings into a table of contents
+    let toc = BrayElem._pairListToTreeList(renderState.headings,
+        function(x, y) { return `<ul>\n<li>${x}</li>\n${y.join('\n')}</ul>\n`; }
+    ).join('');
+    let ret = renderState.builder.join('');
+    ret = ret.replace(/\[@TOC@\]/, toc);
+    if (outer) {
+      ret = ret.replace('[@PRE-TOC@]', '');
+    }
+    return ret;
+  }
+  // Don't call this directly from outside renderToString, please
+  // cf. https://stackoverflow.com/questions/22156326/private-properties-in-javascript-es6-classes
+  _renderToStringInner(renderState) {
+    if (!BrayElem.isString(this.type)) {
+      throw 'BrayElem.renderInner -- expected string element type; this should not happen ...';
+    }
+    let attrBuilder = [];
+    for (let k in this.props) {
+      if (!this.props.hasOwnProperty(k) || k === 'children') {
+        continue;
+      }
+      let rhs = this.props[k] + ''; // make this a string
+      attrBuilder.push(` ${k}="${BrayElem.escapedString(rhs)}"`);
+    }
+    let attrString = attrBuilder.join('');
+    if (this.props.children.length < 1) {
+      BrayElem._smartBuilderPush(renderState, `<${this.type}${attrString}/>`);
+    }
+    else {
+      if (this.type === BrayElem.Fragment) {
+        this._renderChildrenToString(renderState, this.props.children);
+      }
+      else {
+        renderState.processHeader(this);
+        BrayElem._smartBuilderPush(renderState, `<${this.type}${attrString}>`);
+        this._renderChildrenToString(renderState, this.props.children);
+        BrayElem._smartBuilderPush(renderState, `</${this.type}>`);
+      }
+    }
+  }
+  _renderChildrenToString(renderState, children) {
+    children.forEach(kid => {
+      if (BrayElem.isString(kid)) {
+        BrayElem._smartBuilderPush(renderState, kid);
+      }
+      // an array is from fragments?
+      else if (BrayElem.isArray(kid)) {
+        this._renderChildrenToString(renderState, kid);
+      }
+      else {
+        kid._reduceHead()._renderToStringInner(renderState);
+      }
+    });
+  }
+  // 'sax' here is the sax-js namespace (sax.parser will be created for us by XmlHandler)
+  // retrieved 4 Feb 2022 -- last change 5aee216 on Jun 22, 2017
+  // https://github.com/isaacs/sax-js
+  // https://raw.githubusercontent.com/isaacs/sax-js/master/lib/sax.js
+  // https://github.com/isaacs/sax-js/blob/master/lib/sax.js
+  //
+  // 'xmlString' is the XML document as a single string (one XML element (with children), sans DOCTYPE, sans ?xml)
+  //
+  // 'tagMap' is an optional object with keys (strings) mapping tag names to either strings or
+  // function components. Tag name strings with no matching key in 'tagMap' will be left as is.
+  // Function components here mean functions that take a single argument, props (with props.children),
+  // and return a BrayElem. The result of these optional function components (BrayElem(s)) can then be
+  // rendered back to an XML string via BrayElem.renderToString()
+  static fromXmlString(sax, xmlString, tagMap) {
+    tagMap = tagMap || {};
+    const handler = new XmlHandler(sax, tagMap);
+    // parse xmlString and create a giant tree of BrayElems
+    return handler.parseXmlString(xmlString);
   }
 }
